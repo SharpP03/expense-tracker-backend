94e3a5106e42262351b0335e36827f61fbd1f9a1
SharpP03 <Patrykstoncel186@wp.pl>
2026-03-03 00:06:23 +0100

    Custom method setup - api/info endpoint

---
+++ src/main/java/pk/sp/pasir_stoncel_patryk/PasirStoncelPatrykApplication.java
@@ -0,0 +1,13 @@
+package pk.sp.pasir_stoncel_patryk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasirStoncelPatrykApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasirStoncelPatrykApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pk/sp/pasir_stoncel_patryk/PasirStoncelPatrykApplicationTests.java
@@ -0,0 +1,13 @@
+package pk.sp.pasir_stoncel_patryk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasirStoncelPatrykApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
